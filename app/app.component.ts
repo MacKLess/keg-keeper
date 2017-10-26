c57b198301959cd88ea4117365478734afd3814e
L.Devin MacKrell <ldmackrell@gmail.com>
2017-10-26 16:14:50 -0700

    Update app to have correct data types in the master beer list.

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,27 +15,26 @@ export class AppComponent {
   selectedBeer = null;
 
   masterBeerList: Beer[] = [
-    new Beer('Franziskaner Hefe-Weisse', 'Spaten-Franziskaner-Brau', '7', '5.00%'),
-    new Beer('1664 Blanc', 'Kronenbourg', '8', '5.00%'),
-    new Beer('Alaskan Amber', 'Alaskan Brewing', '6', '5.0%'),
-    new Beer('Anchor Steam', 'Anchor', '6', '4.9%'),
-    new Beer('Beamish Stout', 'Beamish', '7', '3.8%'),
-    new Beer('Blue Moon White', 'Blue Moon', '7', '5.4%'),
-    new Beer('Heineken Light', 'Heineken', '5', '3.5%'),
-    new Beer('Black Rabbit Porter', 'McMenamins', '7', '5.5%'),
-    new Beer('Olde English 800 Ice', 'Olde English 800', '6', '7.9%'),
-    new Beer('Pabst', 'Pabst', '5', '5.0%'),
-    new Beer('Pilsner Uruqell', 'Pilsner Uruqell', '6', '4.3%'),
-    new Beer('India Pale Ale', 'Pyramid', '6', '6.7%'),
-    new Beer('Tilted Kilt', 'Pyramid', '6', '6.3%'),
-    new Beer('Redhook Nut Brown', 'Redhook', '6', '5.6%'),
-    new Beer('Sapporo Reserve', 'Sapporo', '7', '5.2%'),
-    new Beer('Arrogant Bastard Ale', 'Stone Brewing', '7', '7.2%'),
-    new Beer('Ruination IPA', 'Stone Brewing', '6', '7.7%'),
-    new Beer('Tsingtao', 'Tsingtao', '6', '4.7%'),
-    new Beer('Drop Top Amber Ale', 'Widmer', '5', '4.9%')
+    new Beer('Franziskaner Hefe-Weisse', 'Spaten-Franziskaner-Brau', 7, 5.0),
+    new Beer('1664 Blanc', 'Kronenbourg', 8, 5.0),
+    new Beer('Alaskan Amber', 'Alaskan Brewing', 6, 5.0),
+    new Beer('Anchor Steam', 'Anchor', 6, 4.9),
+    new Beer('Beamish Stout', 'Beamish', 7, 3.8),
+    new Beer('Blue Moon White', 'Blue Moon', 7, 5.4),
+    new Beer('Heineken Light', 'Heineken', 5, 3.5),
+    new Beer('Black Rabbit Porter', 'McMenamins', 7, 5.5),
+    new Beer('Olde English 800 Ice', 'Olde English 800', 6, 7.9),
+    new Beer('Pabst', 'Pabst', 5, 5.0),
+    new Beer('Pilsner Uruqell', 'Pilsner Uruqell', 6, 4.3),
+    new Beer('India Pale Ale', 'Pyramid', 6, 6.7),
+    new Beer('Tilted Kilt', 'Pyramid', 6, 6.3),
+    new Beer('Redhook Nut Brown', 'Redhook', 6, 5.6),
+    new Beer('Sapporo Reserve', 'Sapporo', 7, 5.2),
+    new Beer('Arrogant Bastard Ale', 'Stone Brewing', 7, 7.2),
+    new Beer('Ruination IPA', 'Stone Brewing', 6, 7.7),
+    new Beer('Tsingtao', 'Tsingtao', 6, 4.7),
+    new Beer('Drop Top Amber Ale', 'Widmer', 5, 4.9)
   ]
-}
 
   // editBeer(clickedBeer) {
   //   this.selectedBeer = clickedBeer;
@@ -48,5 +47,4 @@ export class AppComponent {
   addBeer(newBeerFromChild: Beer) {
     this.masterBeerList.push(newBeerFromChild);
   }
-
 }
--- app/beer-list.component.ts
+++ app/beer-list.component.ts
@@ -3,27 +3,28 @@ import { Beer } from './beer.model';
 
 @Component({
   selector: 'beer-list',
-  template: `
-    <div *ngIf="beer">
-      <h2>{{beer.name}} details!</h2>
-      <div><label>brand: </label>{{beer.brand}}
-      </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="beer.name" placeholder="name">
-      </div>
-      <div>
-        <label>price: </label>{{beer.price}}</div>
-        <input [(ngModel)]="beer.price" placeholder="price">
-      </div>
-      <div>
-        <label>alcohol content (ABV): </label>{{beer.ABV}}</div>
-        <input [(ngModel)]="beer.ABV" placeholder="ABV">
-      </div>
-    </div>
-  `
+  // template: `
+  //   <div *ngIf="beer">
+  //     <h2>{{beer.name}} details!</h2>
+  //     <div><label>brand: </label>{{beer.brand}}
+  //     </div>
+  //     <div>
+  //       <label>name: </label>
+  //       <input [(ngModel)]="beer.name" placeholder="name">
+  //     </div>
+  //     <div>
+  //       <label>price: </label>{{beer.price}}</div>
+  //       <input [(ngModel)]="beer.price" placeholder="price">
+  //     </div>
+  //     <div>
+  //       <label>alcohol content (ABV): </label>{{beer.ABV}}</div>
+  //       <input [(ngModel)]="beer.ABV" placeholder="ABV">
+  //     </div>
+  //   </div>
+  // `
 })
 
-export class BeerDetailComponent {
-  @Input() beer: Beer;
+export class BeerListComponent {
+  @Input() childBeerList: Beer[];
+  // @Output() clickSender = new EventEmitter();
 }
